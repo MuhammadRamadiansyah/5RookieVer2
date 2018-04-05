3a36047e0a4d427c21c85389417502d071cb9155
richoandy <richoandy@gmail.com>
2018-04-05 19:03:33 +0700

    steam API

--- server/routes/steam.js
+++ server/routes/steam.js
@@ -1,7 +1,10 @@
-// const express = require('express');
-// const router = express.Router();
-// const steamController = require('../controllers/steam.controller.js')
+const express = require('express');
+const router = express.Router();
+const steamController = require('../controllers/steam.controller.js')
 
-// router.get('/', steamController.testing)
+// router.get('/', function(req, res){
+//     res.send("testing")
+// })
+router.get('/getProfile/:steamId', steamController.getSteamProfile);
 
-// module.exports = router;
+module.exports = router;
